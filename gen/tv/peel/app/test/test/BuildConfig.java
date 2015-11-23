345528e0cb6c69903649092b40378b0cde803032
rohitha-thammineni <rohitha.thammineni@peel.com>
2015-11-23 12:55:24 +0530

    xyz

---
+++ gen/tv/peel/app/test/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tv.peel.app.test.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/temp/sample.java
@@ -0,0 +1,55 @@
+package temp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+public class sample {
+
+ public static void main (String args []) throws IOException, InterruptedException {
+  fun();
+}
+
+ static void fun () throws IOException, InterruptedException {
+  String[] command =
+      {
+          "cmd",
+      };
+      Process p = Runtime.getRuntime().exec(command);
+      new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+      new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+      PrintWriter stdin = new PrintWriter(p.getOutputStream());
+      stdin.println("cd /");
+      stdin.println("cd Temp");
+      stdin.println("adb shell dumpsys meminfo tv.peel.app.test > naveedjava.txt");
+      // write any other commands you want here
+      stdin.close();
+      int returnCode = p.waitFor();
+      System.out.println("Return code = " + returnCode);
+ }
+ }
+
+class SyncPipe implements Runnable
+{
+public SyncPipe(InputStream istrm, OutputStream ostrm) {
+      istrm_ = istrm;
+      ostrm_ = ostrm;
+  }
+  public void run() {
+      try
+      {
+          final byte[] buffer = new byte[1024];
+          for (int length = 0; (length = istrm_.read(buffer)) != -1; )
+          {
+              ostrm_.write(buffer, 0, length);
+          }
+      }
+      catch (Exception e)
+      {
+          e.printStackTrace();
+      }
+  }
+  private final OutputStream ostrm_;
+  private final InputStream istrm_;
+}
